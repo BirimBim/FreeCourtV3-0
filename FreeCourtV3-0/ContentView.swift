eb73e4959e30dece0123f99d785483589ca6f4c5
Bruno <bruno.l.soterio@gmail.com>
2023-03-23 18:46:13 -0700

    Initial Commit

---
+++ FreeCourtV3-0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FreeCourtV3-0
+//
+//  Created by Bruno Luciano Soterio Ramalho on 2023-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreeCourtV3-0/FreeCourtV3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  FreeCourtV3_0App.swift
+//  FreeCourtV3-0
+//
+//  Created by Bruno Luciano Soterio Ramalho on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct FreeCourtV3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
